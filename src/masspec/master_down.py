44164df1c9585cbb13bcc3609314098d2f2a382c
Sebastian Monzon <jsmonzon@ucsc.edu>
2026-02-11 11:24:56 -0500

    running the stellar halo code on mass spec

---
+++ src/masspec/master_down.py
@@ -0,0 +1,42 @@
+import subprocess
+import json
+import numpy as np
+import os
+
+# Define your global variables here
+config = {
+    "location": "server",
+    "N_cpus": 16,
+    "merger_crit": -2,
+    "fesc": 0.2,
+    "scatter": True}
+
+range = np.arange(11, 14.2, 0.2) # the discrete mass bins
+mass_bins = np.char.mod('%.1f', range) # conver to strings
+datadir = "../../netb/vdbosch/jsm99/data/mass_spec/DF_down/" # all the files are saved here!
+
+h5_blocks = [] # a list of all the text files
+
+for bin in mass_bins:
+    file_list = bin+"_files.txt" # add the mass bin to the list
+    h5_blocks.append(file_list)
+
+    with open(file_list, "a") as f:
+        for filename in os.listdir(datadir):
+            if bin in filename and filename.endswith('evo.npz'): #save all of the files to this block
+                writeout = datadir+filename
+                f.write(writeout + "\n")
+
+for h5 in h5_blocks:
+
+    config["datafiles"] = h5 
+
+    # Write the configuration to a JSON file
+    with open("config.json", "w") as f:
+        json.dump(config, f)
+
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+    subprocess.run(["python", "run_stellarhalo_down.py"])
---
+++ src/masspec/run_stellarhalo_up.py
@@ -0,0 +1,59 @@
+import numpy as np
+import sys
+import os
+import multiprocessing as mp
+import h5py
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+with open(config["datafiles"], "r") as f:
+    files = f.read().splitlines()
+
+# Define paths based on location
+location = config["location"]
+if location == "server":
+    parentdir = "/home/jsm99/SatGen/mcmc/src/"
+
+sys.path.insert(0, parentdir)
+import jsm_stellarhalo 
+
+hf_prefix = config["datafiles"].split(".txt")[0]
+save_name = "/home/jsm99/data/mass_spec/DF_up/"+hf_prefix
+
+Ntrees = len(files)
+print("Reading in", Ntrees, "trees")
+print("---------")
+print("saving to", save_name)
+
+def process_file(file_i):
+    try:
+        tree_i = jsm_stellarhalo.Tree_Reader(file=file_i, merger_crit=config["merger_crit"], fesc=config["fesc"], scatter=config["scatter"], verbose=False)
+        return tree_i.write_out_ash()
+    except Exception as e:
+        print(f"Error processing {file_i}: {e}")
+        return None
+    
+if __name__ == "__main__":
+
+    with mp.Pool(processes=config["N_cpus"]) as pool:
+        results = pool.map(process_file, files) 
+
+    # Filter out None results safely
+    valid_entries = [entry for entry in results if isinstance(entry, dict) and "tree_index" in entry]
+
+    dict_dict = {
+        entry["tree_index"]: {k: v for k, v in entry.items() if k != "tree_index"}
+        for entry in valid_entries
+    }
+    
+    with h5py.File(f"{save_name}.h5", "w") as f: # saving the dictionary for the surviving population!
+        for sim_name, attributes in dict_dict.items():
+            sim_group = f.create_group(sim_name)  # Create a group for each simulation
+            
+            for attr_name, data in attributes.items():
+                if np.isscalar(data) or (isinstance(data, np.ndarray) and data.shape == ()):
+                    sim_group.create_dataset(attr_name, data=data)  # no compression
+                else:
+                    sim_group.create_dataset(attr_name, data=data, compression="gzip", compression_opts=9)
