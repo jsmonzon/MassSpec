4100f6793232c233f2a14a7608cadd0c17cd24da
Sebastian Monzon <jsmonzon@ucsc.edu>
2026-06-08 14:08:00 -0400

    changing the way we measure MMs

--- src/MW/fiducial/master.py
+++ src/MW/fiducial/master.py
@@ -8,13 +8,13 @@ config = {
     "merger_crit": -2,
     "fesc": 0.2,
     "scatter": True,
-    "mass_cut": 1.2*10**8}
+    "mass_cut": 1e9}
 
 # Write the configuration to a JSON file
 with open("config.json", "w") as f:
     json.dump(config, f)
 
-scripts = []
+scripts = ["run_S0.py", "run_S15.py", "run_S30.py"]
 
 for script in scripts:
 
