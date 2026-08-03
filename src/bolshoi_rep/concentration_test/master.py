afb2406dfa64696516efcd5931a58e2f1bbef47a
Sebastian Monzon <jsmonzon@ucsc.edu>
2026-08-03 17:06:45 -0400

    pushing the new bolshoi_rep test

---
+++ src/bolshoi_rep/concentration_test/master.py
@@ -0,0 +1,28 @@
+import subprocess
+import json
+import numpy as np
+
+# Define your global variables here
+config = {
+    "location": "server",
+    "N_cpus": 16,
+    "seed": 42,
+    "mass_cut": 7.75e10}
+
+# Write the configuration to a JSON file
+with open("config.json", "w") as f:
+    json.dump(config, f)
+
+Nparticles = [1e5, 1e6, 1e7]
+
+for Npart in Nparticles:
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(f"Running with log Nparticles = {np.log10(Npart)}")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+    config["Nparticles"] = Npart
+
+    with open("config.json", "w") as f:
+        json.dump(config, f)
+
+    subprocess.run(["python", "run_sample.py"])
